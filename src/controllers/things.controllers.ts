2a9afee8142d990d92ab85a179e3f418d32bac97
mariano quiroga <mariano.eq@gmail.com>
2023-02-24 18:50:19 +0100

    Create controllers methods

---
+++ src/controllers/things.controllers.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+export class ThingsControllers {
+  constructor() {}
+
+  getAllThings(req: Request, res: Response) {}
+  getThingById(req: Request, res: Response) {}
+  updateThing(req: Request, res: Response) {}
+  deleteThing(req: Request, res: Response) {}
+}
