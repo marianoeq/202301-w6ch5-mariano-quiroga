005e372c981de85a213532dae7b830c891d8263f
mariano quiroga <mariano.eq@gmail.com>
2023-02-26 15:42:23 +0100

    Done all methods

--- src/repository/things.file.repo.ts
+++ src/repository/things.file.repo.ts
@@ -1,7 +1,10 @@
 import fs from 'fs/promises';
 
 const file = './data/data.json';
-
+type knowledge = {
+  id: number;
+  name: string;
+};
 export class ThingsFileRepo {
   read() {
     return fs.readFile(file, { encoding: 'utf-8' }).then((data) => {
@@ -9,11 +12,47 @@ export class ThingsFileRepo {
     });
   }
 
-  write() {
-    return fs.readFile(file, { encoding: 'utf-8' }).then((data) => {
-      return;
+  async readById(id: knowledge['id']) {
+    const data = await fs.readFile(file, 'utf-8');
+
+    const dataParsed = JSON.parse(data);
+    const result = dataParsed.find((item: knowledge) => item.id === id);
+    return result;
+  }
+
+  write(newThing: knowledge) {
+    return fs.readFile(file, 'utf-8').then((data) => {
+      const dataParsed: knowledge[] = JSON.parse(data);
+
+      const id = dataParsed.length;
+
+      newThing.id = id + 1;
+
+      const dataAdded = JSON.stringify([...dataParsed, newThing]);
+
+      return fs.writeFile(file, dataAdded, 'utf-8');
     });
   }
-  update() {}
-  delete() {}
+  update(id: knowledge['id'], newThing: knowledge) {
+    return fs.readFile(file, 'utf-8').then((data) => {
+      const dataParsed: knowledge[] = JSON.parse(data);
+      newThing.id = id;
+
+      const idFound = dataParsed.map((item: knowledge) =>
+        item.id === id ? (item = newThing) : item
+      );
+
+      const finalData = JSON.stringify(idFound);
+      return fs.writeFile(file, finalData, 'utf-8');
+    });
+  }
+
+  async delete(id: knowledge['id']) {
+    const data = await fs.readFile(file, 'utf-8');
+    const dataParsed = JSON.parse(data);
+    const dataFiltered = dataParsed.filter((item: knowledge) => item.id !== id);
+    const finalData = JSON.stringify(dataFiltered);
+    console.log(finalData);
+    return fs.writeFile(file, finalData, 'utf-8');
+  }
 }
